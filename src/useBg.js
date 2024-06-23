6125b2436bb41623a06227c74bf323bd9ad0c2b7
secret <secretx500@qq.com>
2024-06-23 23:02:21 +0800

    fix: vite import image path error

--- src/useBg.js
+++ src/useBg.js
@@ -1,8 +1,8 @@
 export function useBg() {
   const imageModules = import.meta.glob("@/assets/bg/*.jpg", { eager: true });
-  console.log(imageModules);
   function refreshBackground() {
-    const images = Object.keys(imageModules);
+    const images = Object.values(imageModules).map(it => it.default);
+    console.log(images);
     const index = Math.floor(Math.random() * images.length)
     const image = images[index];
     document.getElementById("app").style.backgroundImage = `url(${image})`;
