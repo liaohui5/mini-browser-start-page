6629cd5fe5d5c050881ef59b9524d37291b10d93
secret <liaohui5@foxmail.com>
2024-09-12 16:45:03 +0000

    fix gitlab cicd config file name error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/mini-browser-start-page/",
+  base: "/mini-browser-start-page/",
   plugins: [vue()],
   resolve: {
     alias: {
