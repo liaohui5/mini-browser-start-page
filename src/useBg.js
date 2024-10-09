aa60aff454d2e37e28c8bd40ffb6f4be0f1872a1
secret <secretx500@outlook.com>
2024-10-10 02:11:58 +0800

    feat: :sparkles: add some backgrond images

--- src/useBg.js
+++ src/useBg.js
@@ -1,11 +1,47 @@
-import image1 from "@/assets/bg/0.jpg";
-import image2 from "@/assets/bg/1.jpg";
-import image3 from "@/assets/bg/2.jpg";
-import image4 from "@/assets/bg/3.jpg";
-import image5 from "@/assets/bg/4.jpg";
+import image0 from "@/assets/bg/0.jpg"
+import image1 from "@/assets/bg/1.jpg"
+import image2 from "@/assets/bg/2.jpg"
+import image3 from "@/assets/bg/3.jpg"
+import image4 from "@/assets/bg/4.jpg"
+import image5 from "@/assets/bg/5.jpg"
+import image6 from "@/assets/bg/6.jpg"
+import image7 from "@/assets/bg/7.jpg"
+import image8 from "@/assets/bg/8.jpg"
+import image9 from "@/assets/bg/9.jpg"
+import image10 from "@/assets/bg/10.jpg"
+import image11 from "@/assets/bg/11.jpg"
+import image12 from "@/assets/bg/12.jpg"
+import image13 from "@/assets/bg/13.jpg"
+import image14 from "@/assets/bg/14.jpg"
+import image15 from "@/assets/bg/15.jpg"
+import image16 from "@/assets/bg/16.jpg"
+import image17 from "@/assets/bg/17.jpg"
+import image18 from "@/assets/bg/18.jpg"
+import image19 from "@/assets/bg/19.jpg"
 
 export function useBg() {
-  const images = [image1, image2, image3, image4, image5];
+  const images = [
+    image0,
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+  ];
 
   const defaultBgKey = "__default_bg_image__";
   const storage = window.localStorage;
